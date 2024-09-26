eee8a44e2987ee986f5f58811be589e45dd88ed6
Shea <s.coughlin037@mybvc.ca>
2024-09-26 12:52:39 -0600

    Added 3.4 and 3.3

--- Program.cs
+++ Program.cs
@@ -41,34 +41,151 @@ class Program
         Console.WriteLine("This is the function for Learning Activity 3.2!");
         Console.ReadKey();
     }
+
     static void LearningActivity33()
     {
-        Console.WriteLine("This is the function for Learning Activity 3.3!");
+        char[,] board = new char[3, 3]
+        {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
+
+        char currentPlayer = 'X';
+        bool gameWon = false;
+        bool gameDraw = false;
+
+        while (!gameWon && !gameDraw)
+        {
+            DisplayBoard(board);
+            PlayerMove(board, currentPlayer);
+            gameWon = CheckWin(board, currentPlayer);
+            gameDraw = CheckDraw(board);
+
+            if (gameWon)
+            {
+                DisplayBoard(board);
+                Console.WriteLine($"Player {currentPlayer} wins!");
+            }
+            else if (gameDraw)
+            {
+                DisplayBoard(board);
+                Console.WriteLine("The game is a draw!");
+            }
+            else
+            {
+                currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+            }
+        }
+        Console.ReadKey();
     }
+
+    static void DisplayBoard(char[,] board)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(board[i, j]);
+                if (j < 2) Console.Write("|");
+            }
+            Console.WriteLine();
+            if (i < 2) Console.WriteLine("-----");
+        }
+    }
+
+    static void PlayerMove(char[,] board, char player)
+    {
+        int move;
+        bool validMove = false;
+        do
+        {
+            Console.WriteLine($"Player {player}, enter your move (1-9):");
+            move = int.Parse(Console.ReadLine()) - 1;
+            int row = move / 3;
+            int col = move % 3;
+            if (board[row, col] != 'X' && board[row, col] != 'O')
+            {
+                board[row, col] = player;
+                validMove = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid move!");
+            }
+
+        } while (!validMove);
+    }
+
+    static bool CheckWin(char[,] board, char player)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if ((board[i, 0] == player && board[i, 1] == player && board[i, 2] == player) ||
+                (board[0, i] == player && board[1, i] == player && board[2, i] == player))
+                return true;
+        }
+        if ((board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) ||
+            (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player))
+            return true;
+
+        return false;
+    }
+
+    static bool CheckDraw(char[,] board)
+    {
+        foreach (char cell in board)
+        {
+            if (cell != 'X' && cell != 'O')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
     static void LearningActivity34()
     {
-        int number;
-        bool success = int.TryParse(Console.ReadLine(), out number); //example code
-        Console.WriteLine("This is the function for Learning Activity 3.4!");
+        int age;
+        bool isValid;
+
+        do
+        {
+            Console.WriteLine("Please enter your age!");
+            string input = Console.ReadLine();
+
+            isValid = int.TryParse(input, out age);
+            if (isValid && age > 10 && age < 50)
+            {
+                Console.WriteLine("Thank you!");
+                Console.ReadKey();
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Please try again.");
+            }
+        } while (true);
     }
 
     static void LearningActivity35()
-    {
-        int[] highScores = { 1272700, 1271100, 1243000, 1218000, 1214300, 1210800, 1210400, 1206800, 1178400 };
+        {
+            int[] highScores = { 1272700, 1271100, 1243000, 1218000, 1214300, 1210800, 1210400, 1206800, 1178400 };
 
-        int averageScore = CalculatedAverage(highScores);
-        double standardDeviation = CalculatedAverageDev(highScores, averageScore);
+            int averageScore = CalculatedAverage(highScores);
+            double standardDeviation = CalculatedAverageDev(highScores, averageScore);
 
-        Console.WriteLine("Average Rounded Score" + averageScore);
-        Console.WriteLine("Average Deviation " + standardDeviation);
-        Console.ReadKey();
+            Console.WriteLine("Average Rounded Score" + averageScore);
+            Console.WriteLine("Average Deviation " + standardDeviation);
+            Console.ReadKey();
 
 
-        Console.WriteLine("This is the function for Learning Activity 3.5!");
-    }
+            Console.WriteLine("This is the function for Learning Activity 3.5!");
+        }
 
-    public static int CalculatedAverage(int[] scores)
+     static int CalculatedAverage(int[] scores)
     {
         int sum = 0;
         foreach (int score in scores)
@@ -78,7 +195,7 @@ class Program
         return (int)Math.Round((double)sum / scores.Length);
     }
 
-    public static double CalculatedAverageDev(int[] scores, int mean)
+     static double CalculatedAverageDev(int[] scores, int mean)
     {
         double sumOfSquares = 0;
         foreach (int score in scores)
@@ -89,6 +206,4 @@ class Program
         double variance = sumOfSquares / scores.Length;
         return Math.Sqrt(variance);
     }
-
-
 }
